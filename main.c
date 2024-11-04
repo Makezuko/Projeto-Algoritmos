55ae51524b4bb3539272ea3f2b6c9434885a1b2c
Matias Amma <matiasamma@gmail.com>
2024-11-03 21:57:54 -0300

    Projeto de Algoritmos de ordenação

--- main.c
+++ main.c
@@ -1,4 +1,32 @@
 #include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+
+// Selection Sort
+void SelectionSort(){
+
+}
+// Insertion Sort
+void Sort(){
+
+}
+// Bubble Sort
+void bubbleSort(){
+
+}
+// Merge Sort
+void MergeSort(){
+
+}
+// Quick Sort
+void QuickSort(){
+
+}
+// Heap Sort
+void HeapSort(){
+
+}
+
 
 int main(){
     return 0;
